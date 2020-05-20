8f30e57bc89a128a08646a98d13f60ac9ee215df
Julian <cusco2891@gmail.com>
2020-05-20 17:05:45 -0400

    react slider working

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import "./Slider.scss";
 import ImgComp from "./ImgComp";
 import i1 from "../pics/ductwork1.jpg";
+import i2 from "../pics/fantasy.jpg";
+import i3 from "../pics/fantasy2.jpeg";
+import i4 from "../pics/fantasy3.jpeg";
+import i5 from "../pics/fantasy4.jpeg";
+
+
+
 
 
 
 
 function Slider() {
 
-    let sliderArr = [<ImgComp src={i1} />, 2, 3, 4, 5];
+    let sliderArr = [<ImgComp src={i1}/>, <ImgComp src={i2}/>, <ImgComp src={i3}/>, <ImgComp src={i4}/>, <ImgComp src={i5}/>];
     const [x, setX] = useState(0);
     const goLeft = () => {
         // console.log(x);
